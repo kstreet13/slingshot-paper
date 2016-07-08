5bc66110f73401a4cb722ea70fb409d5c65dfd9f
Kelly Street <kstreet@berkeley.edu>
2016-07-08 14:08:09 -0700

    started looking at embryo dataset for potential incorporation. Working on explanation for simultaneous principal curves.

---
+++ embryoDataset.R
@@ -0,0 +1,30 @@
+counts <- read.table('~/Downloads/counts.txt')
+counts <- counts[rowMeans(counts)>10,]
+meta <- read.table('~/Downloads/metadata.txt',header = TRUE)
+require(RColorBrewer)
+cc <- c(brewer.pal(9, "Set1")[-1], brewer.pal(8, "Set2"), brewer.pal(9, "Set3"), brewer.pal(6, "Dark2"))
+batch <- sapply(as.character(meta[,1]),function(s){unlist(strsplit(s,split='[.]'))[2]})
+meta$batch <- factor(batch); rm(batch)
+
+good <- meta$cluster != 'grey'
+depth <- log1p(colSums(counts))
+gnames <- read.csv('~/Downloads/mart_export.txt',header = T)
+
+require(scone)
+norm <- DESEQ_FN(counts)
+norm <- FQ_FN(counts)
+
+data("housekeeping")
+housekeeping <- tolower(housekeeping[,1])
+hk <- as.character(gnames[gnames[,2] %in% housekeeping,1])
+hk <- hk[hk %in% rownames(norm)]
+
+ruv <- RUVg(round(as.matrix(norm)), hk, 1)
+
+X <- prcomp(t(log1p(ruv$normalizedCounts)))$x
+require(fastICA)
+ica <- fastICA(t(log1p(ruv$normalizedCounts)),n.comp = 3)
+X <- prcomp(t(log1p(norm)))$x
+
+require(rgl)
+plot3d(X[,1:3], col=as.character(meta$cluster), size = 3)
